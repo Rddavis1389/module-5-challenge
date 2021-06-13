9cc83ea73bd0f7670b340e005b7d03ef4bbaa856
Rddavis1389 <rddavis1389@yahoo.com>
2021-06-12 21:13:52 -0600

    calender setup

---
+++ script.js
@@ -0,0 +1,27 @@
+var currentDate = moment().format('dddd, MMMM Do, YYYY')
+
+$("#currentDay").text(currentDate);
+
+var currentHour = moment().hour();
+
+$(".blockHour").each(function(){
+    var blockHour = $(this).attr("id");
+    console.log(blockHour)
+
+    if(blockHour < currentHour){
+        $(this).addClass("past")
+        $(this).removeClass("present")
+        $(this).removeClass("future")
+    }
+    else if(blockHour == currentHour){
+        $(this).addClass("present")
+        $(this).removeClass("past")
+        $(this).removeClass("future")
+    }
+    else {
+        $(this).addClass("future")
+        $(this).removeClass("past")
+        $(this).removeClass("present")
+    }
+
+})
