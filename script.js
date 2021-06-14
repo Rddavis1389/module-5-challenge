6c23019ae7ff053fb551ea9393367331aea64b4e
Rddavis1389 <rddavis1389@yahoo.com>
2021-06-13 22:02:08 -0600

    saveBtn

--- script.js
+++ script.js
@@ -4,36 +4,56 @@ $("#currentDay").text(currentDate);
 
 var currentHour = moment().hour();
 
-$(".blockHour").each(function(){
-    var blockHour = $(this).attr("id");
-    console.log(blockHour)
+$(document).ready(function () {
+    // saveBtn click listener 
+    $(".saveBtn").on("click", function () {
+        // Get nearby values of the description in JQuery
+        var text = $(this).siblings(".blockHour").val();
+        var time = $(this).parent().attr("id");
 
-    if(blockHour < currentHour){
-        $(this).addClass("past")
-        $(this).removeClass("present")
-        $(this).removeClass("future")
-    }
-    else if(blockHour == currentHour){
-        $(this).addClass("present")
-        $(this).removeClass("past")
-        $(this).removeClass("future")
-    }
-    else {
-        $(this).addClass("future")
-        $(this).removeClass("past")
-        $(this).removeClass("present")
-    }
+        // Save text in local storage
+        localStorage.setItem(time, text);
+    })
+
+    function timeTracker() {
+        //get current number of hours.
+        var currentHour = moment().hour();
+
+        // loop over time blocks
+        $(".blockHour").each(function () {
+            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+            $(".blockHour").each(function () {
+                var blockHour = $(this).attr("id");
+                console.log(blockHour)
+
+                if (blockHour < currentHour) {
+                    $(this).addClass("past")
+                    $(this).removeClass("present")
+                    $(this).removeClass("future")
+                }
+                else if (blockHour == currentHour) {
+                    $(this).addClass("present")
+                    $(this).removeClass("past")
+                    $(this).removeClass("future")
+                }
+                else {
+                    $(this).addClass("future")
+                    $(this).removeClass("past")
+                    $(this).removeClass("present")
+                }
+            })
+
+            // local storage
+            $("#9 .blockHour").val(localStorage.getItem("9"));
+            $("#10 .blockHour").val(localStorage.getItem("10"));
+            $("#11 .blockHour").val(localStorage.getItem("11"));
+            $("#12 .blockHour").val(localStorage.getItem("12"));
+            $("#1 .blockHour").val(localStorage.getItem("1"));
+            $("#2 .blockHour").val(localStorage.getItem("2"));
+            $("#3 .blockHour").val(localStorage.getItem("3"));
+            $("#4 .blockHour").val(localStorage.getItem("4"));
+            $("#5 .blockHour").val(localStorage.getItem("5"));
+
+            timeTracker();
 })
-
-// local storage
-$("#9 .blockHour").val(localStorage.getItem("9"));
-$("#10 .blockHour").val(localStorage.getItem("10"));
-$("#11 .blockHour").val(localStorage.getItem("11"));
-$("#12 .blockHour").val(localStorage.getItem("12"));
-$("#1 .blockHour").val(localStorage.getItem("1"));
-$("#2 .blockHour").val(localStorage.getItem("2"));
-$("#3 .blockHour").val(localStorage.getItem("3"));
-$("#4 .blockHour").val(localStorage.getItem("4"));
-$("#5 .blockHour").val(localStorage.getItem("5"));
-
-timeTracker();
